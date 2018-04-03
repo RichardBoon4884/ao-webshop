53099f9739dda19c03c652df7bd8735fb42dd878
Richard Boon <ric.ric.boon@gmail.com>
2018-04-03 14:48:51 +0200

    Added comments

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,15 +9,19 @@ class ProductController extends Controller
 {
     public function index()
     {
+        // Get the data from the database
         $products = DB::table('products')->get();
 
+        // Render the web page
         return view('product.index', ['products' => $products]);
     }
 
     public function view($id, $slug = null)
     {
+        // Get the data from the database
         $product = DB::table('products')->where('id', $id)->first();
 
+        // Render the web page
         return view('product.view', ['product' => $product]);
     }
 }
